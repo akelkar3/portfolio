426ecd4c1c875bcf91da0df236e806b87ede6f28
Ankit Kelkar <akelkar3@uncc.edu>
2018-11-22 01:23:43 -0500

    added projects and images

---
+++ js/coverletter.js
@@ -0,0 +1,48 @@
+var ViewModel = function(first, last) {
+    self=this;
+    self.company = ko.observable();
+    self.position = ko.observable();
+    self.JD1=ko.observable(" ");
+    self.JD2=ko.observable(" ");
+    self.JD3=ko.observable(" ");
+    self.JD4=ko.observable(" ");
+self.letter = ko. observable(" ");
+   
+self.create= function(){
+    self.letter(self.company()+"<br/>"+self.position()
+  
+    +"<br/>Dear Recruiting Team,<br/> "
+     
+    +"<br/>I am writing to apply for the Web intern position as posted on your website. My full-stack web development experience, proven creative and quality front-end development, and communication skills make me well suited for this position."
+     
+    +"<br/>I would be proud to work with"+self.company()+", one of the fastest growing companies in USA, through the opportunity which interests me most. I have been working with Infosys Ltd. as a Senior Systems Engineer for a US based client. This experience has helped me in gaining good experience in front-end web technologies- JavaScript, jQuery, AJAX, CSS, Angular JS and object-oriented programming language such as C# as well a very good exposure to relational database SQL Server." 
+    +"<br/>I have been involved in  developing proof of concepts for front end of the deployed web-application. This experience has enabled me to visualize and craft corporate and creative UI/UX."
+    +"<br/>I am eager to work with "+self.company()+   " and I feel my skills will be a valuable contribution."
+     +"<br/>During my experience at Infosys Ltd. I have honed my time management skills, detail oriented approach and on time delivery commitment due to a global customer experience in the corporate setup." 
+    +"<br/>I believe my profound experience in "
+    +"<br/>"+self.JD1()
+    +"<br/>"+self.JD2()
+    +"<br/>"+self.JD3()
+    +"<br/>"+self.JD4()
+    +"<br/>makes me competent for this opportunity"
+    +"<br/>Overall I really enjoy bringing creativity and technology to one another."
+     
+    +"<br/>I am looking forward to your response, please contact me for any additional information."
+    
+    +"<br/>Sincerely,"
+    +"<br/>Ankit Kelkar")
+       
+    $('#letter').append(self.letter ());
+}
+
+
+self.copy = function(){
+    $('#letter').select();
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+}
+} 
+
+ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
--- js/dashboard.js
+++ js/dashboard.js
@@ -4,10 +4,22 @@ var ViewModel = function(first, last) {
   
   var data= [
     {
+      title:"Easy Connect"
+      ,skills:"twillio, NodeJs, Web Services, Android, ExpressJS, Facebook API"
+      ,id:8
+      ,image:'images/easyconnect.jpg'
+    },
+    {
+      title:"SMS Gateway"
+      ,skills:"Google Cloud Messaging,Android, NodeJs, Web Services, ExpressJS, MongoDB, AWS Deployment"
+      ,id:9
+      ,image:'images/smsgateway.jpeg'
+    },
+    {
       title:"Product Visualization"
       ,skills:"Unity3d, C#, Web Services, iOS, ARKit, Android, ARCore"
       ,id:7
-      ,image:''
+      ,image:'images/productvisualisation.jpg'
     },
     {
       id:1,
@@ -43,7 +55,7 @@ var ViewModel = function(first, last) {
   
 ];
 
- var str= "C#, C++, Java, Python, JavaScript, Swift 4.0, Visual Studio, Unity 3D, OpenGL, OpenCV, Android Studio, Eclipse, Xamarin, MATLAB, XCode, Vuforia, AR Core, AR Kit ,.NET MVC, SQL Server 2012, MVVM, Bootstrap, CSS, NodeJS, MongoDB, ExpressJS, AngularJS"
+ var str= "C#, C++, Java, Python, JavaScript, Swift 4.0, Visual Studio, Unity 3D, OpenGL, OpenCV, Android Studio, Eclipse, Xamarin, MATLAB, XCode, Vuforia, AR Core, AR Kit ,.NET MVC, SQL Server 2012, MVVM, Bootstrap, CSS, NodeJS, MongoDB, ExpressJS, AngularJS, AWS Deployment"
  var skilldata= str.split(',');
 self.projects = ko.observableArray(data);
 self.skills= ko.observableArray(skilldata);
@@ -104,6 +116,18 @@ switch (project) {
  $('#modalBody').text('');
  $('#modalBody').append('<p>• Developing an Augmented Reality application for iOS/ Android/ MagicLeap	</p> <p>•	Rendering 3D objects through C# and web service integration. </p> <p>• Compared different technologies available for augmented reality. </p><p>• Implemented continuous integration continuous delivery through Jenkins</p><p> • Implemented MVC framework for Unity3D</p>');
  break;
+ case 8:
+ $('#modalTitle').text('EasyConnect')
+   
+ $('#modalBody').text('');
+ $('#modalBody').append('<p>• Devloped a multi platform tool for reaching out to homeless people	</p> <p>•	Used Facebook API integration. </p> <p>• Won Wolfram Award at Duke University Hackathon. </p><p><a href="https://devpost.com/software/hackduke-xbkcq7">click here to see details.</a></p><iframe width="300"src="https://www.youtube.com/embed/8Jaxl1Wc_2g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+ break;
+ case 9:
+ $('#modalTitle').text('EasyConnect')
+   
+ $('#modalBody').text('');
+ $('#modalBody').append('<p>Created a sms gateway using two components a gateway server and a gateway app. For the developer who wants utilize this gateway. Gateway will generate an API key for all the users to authorize the users and provide them a way to communicate with their registered phone numbers through a gateway server. Where the mapping of the developer, its devices and the webhooks to talk with are stored. Once the developer initiates a communication with gateway server using the message sending api the message is relayed to particular device of the developer. This device has our gateway app which gets a push notification from gateway server and then in turn sends the sms to the respective phone number requested by the developer in the sending api. Whereas when one of these phone numbers receive the sms they talk through the gateway app to our gateway server which figures out which developer is registered with this device/phone number and uses its respective webhook to call the call back api. And the message is relayed to the developer.<p><a href="https://github.com/akelkar3/SMSGatewayAPI">click here to see details.</a></p><iframe width="300" src="https://www.youtube.com/embed/Xsu4XHD3kM8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+ break;
   default:
     break;
 }
@@ -112,3 +136,5 @@ switch (project) {
 } 
 
 ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
+
+
