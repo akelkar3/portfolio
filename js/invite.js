800042459d7be1df7ae0f9e386f5c1701692f9c7
akelkar3 <akelkar3@uncc.edu>
2019-01-08 22:49:58 -0500

    adding rsvp

--- js/invite.js
+++ js/invite.js
@@ -1,5 +1,35 @@
 var ViewModel = function(first, last) {
- 
+    var self= this;
+    self.guests=ko.observable('');
+    self.name =ko.observable('');
+ self.accept = function (params) {
+    if(self.name() == '' )return;
+    if(self.guests() =='')return;
+    console.log('fire');
+    $.ajax({
+        method: "POST",
+        contentType: 'application/json',
+        
+        data: JSON.stringify({
+            name:self.name(),
+            email: self.guests(),
+       }),
+            url: "http:/rsvp.gear.host/api/Loan/RSVP",
+            success: function(result) {
+                //Write your code here
+                console.log(result);
+              
+                },
+            error:
+            function(result) {
+                //Write your code here
+                $.toast({heading:'error',text:'SOME ERROR',icon:'error'});
+                }
+        
+      });
+
+ }
+
 } 
 
 ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
